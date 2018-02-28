20f3b50ea00d60505b20be5473ae10619ad53a77
yadong <yadonghan@hotmail.com>
2018-02-28 16:48:56 -0500

    add my work of in lab part

--- WS05/in-lab/Fraction.h
+++ WS05/in-lab/Fraction.h
@@ -1,17 +1,36 @@
+// Name Yadong Han
+// ID   151799160
+//email yhan61@myseneca.ca
+
 // TODO: header file guard
+#ifndef SICT_FRACTION_H
+#define SICT_FRACTION_H
 
 // TODO: create namespace
-
+namespace sict
+{
 	// TODO: define the Fraction class
 	class Fraction {
 		// TODO: declare instance variables 
-
+		int m_numer;
+		int m_deno;
 		// TODO: declare private member functions
+		int max() const;
+		int min() const;
+		void reduce();
+		int gcd() const;
+
 
 	public:
 		// TODO: declare public member functions
-
+		Fraction();
+		Fraction(int n, int d);
+		bool isEmpty() const;
+		void display() const;
 		// TODO: declare the + operator overload
-
+		Fraction operator+(const Fraction& rhs)const;
 	};
 
+
+}
+#endif
